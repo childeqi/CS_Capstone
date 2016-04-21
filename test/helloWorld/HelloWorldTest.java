f26bf776d8345fd52d6efc6386d2f6aa84b29a35
Brandon Carroll <boc17@pitt.edu>
2016-04-21 16:00:54 -0400

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -1,5 +1,5 @@
-//package helloWorld;
-package GameOfLife;
+package helloWorld;
+//package GameOfLife;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +8,7 @@ import org.junit.Test;
 import uk.co.placona.helloWorld.HelloWorld;
 
 public class HelloWorldTest {
-/*	@Test
+	@Test
 	public void testHelloWorld(){
 		HelloWorld hello = new HelloWorld();
 		assertEquals("Test 1", "Hello World", hello.sayHello());
@@ -25,12 +25,12 @@ public class HelloWorldTest {
 		HelloWorld hello = new HelloWorld();
 		assertEquals("Test 3", "Hello World   ", hello.sayHello());
 	}
-*/	
+/*	
 	@Test
 	public void testLoadButton(){
 		MainPanel _m;
 		LoadButton newButton = new LoadButton(_m);
 		assertNotNull(newButton);
 	}
-	
+*/	
 }
