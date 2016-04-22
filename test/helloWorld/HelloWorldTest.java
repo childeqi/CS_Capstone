adc2d3555735137fe9e6ff49d4b0e3268e1d7506
Nicholas Carone <njc39@pitt.edu>
2016-04-22 08:53:54 -0400

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -15,16 +15,16 @@ public class HelloWorldTest {
 		assertEquals("Test 1.2", "Helllllllo World", hello.sayHello());	
 		
 	}
-//	@Test
-//	public void test2HellowWorld(){
-//		HelloWorld hello = new HelloWorld();
-//		assertEquals("Test 2", "Hello World", hello.sayHello());
-//	}
-  //  @Test
-//	public void test3HellowWorld(){
-//		HelloWorld hello = new HelloWorld();
-//		assertEquals("Test 3", "Hello World   ", hello.sayHello());
-//	}
+	@Test
+	public void test2HellowWorld(){
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Test 2", "Hello World", hello.sayHello());
+	}
+	@Test
+	public void test3HellowWorld(){
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Test 3", "Hello World   ", hello.sayHello());
+	}
 /*	
 	@Test
 	public void testLoadButton(){
