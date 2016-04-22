2840469022477e629c6998551447987a5702f6e8
childeqi <mengqi0424@gmail.com>
2016-04-22 03:05:02 -0400

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -11,7 +11,7 @@ public class HelloWorldTest {
 	@Test
 	public void testHelloWorld(){
 		HelloWorld hello = new HelloWorld();
-		assertEquals("Test 1", "Hello World", hello.sayHello());
+		assertEquals("Test 1", "Hello     World", hello.sayHello());
 	//	assertEquals("Test 1.2", "Helllllllo World", hello.sayHello());	
 		
 	}
